7e2a23c5019ba366135184ffceaeb4547eb085bf
Richard Westenra <rjwestenra@gmail.com>
2018-10-05 19:36:08 +0100

    Move have fun to the end

--- js/transition.js
+++ js/transition.js
@@ -10,7 +10,7 @@ function TransitionsChart(){
   var chart = d3.select(id)
     .attr('width', width);
 
-  x.domain([0, d3.max(data, function(d) { return d; })]);
+  x.domain([0, d3.max(data, d => d)]);
 
   chart.attr('height', barHeight * data.length);
 
@@ -21,25 +21,25 @@ function TransitionsChart(){
       .data(data)
       .enter()
       .append('g')
-      .attr('transform', function(d, i) { return 'translate(0,' + i * barHeight + ')'; });
+      .attr('transform', (d, i) => 'translate(0,' + i * barHeight + ')');
 
     bar.append('rect')
       .attr('width',0)
       .attr('height', barHeight - 1)
       .transition()
       .duration(600)
-      .delay(function (d,i){ return 800 + i * 100;})
-      .attr('width', function(d) { return x(d); });
+      .delay((d, i) => 800 + i * 100)
+      .attr('width', d => x(d));
 
     bar.append('text')
       .attr('class', 'barLabel')
       .attr('x', 0)
       .attr('y', barHeight / 2)
       .attr('dy', '.35em')
-      .text(function(d) { return d; })
+      .text(d => d)
       .transition()
       .duration(600)
-      .delay(function (d,i){ return 800 + i * 100;})
-      .attr('x', function(d) { return x(d) - 3; });
+      .delay((d, i) => 800 + i * 100)
+      .attr('x', d => x(d) - 3);
   };
 }
